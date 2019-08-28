33dc590c1688d2f2278bed11d38ea16b993e4821
Rasmus Andersson <rasmus@notion.se>
2019-08-28 12:06:53 -0700

    fixup index.html

--- misc/build-app.sh
+++ misc/build-app.sh
@@ -11,8 +11,19 @@ rm -rf docs/app.* docs/resources.*
 spawn_monaco_build "$rootdir/docs" && echo "monaco is up-to-date" || true
 
 echo "building ./src/app -> ./docs"
-cd src/app
+pushd src/app > /dev/null
 webpack --mode=production --progress --display=errors-only "--output-path=$rootdir/docs"
 
 wait
+popd > /dev/null
+mv -f docs/resources.* docs/resources.js
+
+# remove "resources" script tag added by webpack
+node <<_JS_
+let fs = require("fs")
+let s = fs.readFileSync("docs/index.html", "utf8")
+s = s.replace(/<script type="text\/javascript" src="resources\.[^\.]+\.js"><\/script>/, "")
+fs.writeFileSync("docs/index.html", s, "utf8")
+_JS_
+
 echo "✓ done"
