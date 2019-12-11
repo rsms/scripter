47b64a6681b9aca1c274076d8b536c42a09b63e7
Rasmus Andersson <rasmus@notion.se>
2019-12-11 11:49:07 -0800

    add log messages to misc/update-figma-type-defs.sh

--- misc/update-figma-type-defs.sh
+++ misc/update-figma-type-defs.sh
@@ -5,3 +5,5 @@ URL=https://www.figma.com/plugin-docs/figma.d.ts
 echo "fetch $URL"
 curl '-#' -o src/figma-plugin/figma.d.ts "$URL"
 cp src/figma-plugin/figma.d.ts src/app/figma-1.0.0.d.ts
+echo "wrote src/figma-plugin/figma.d.ts"
+echo "wrote src/app/figma-1.0.0.d.ts"
