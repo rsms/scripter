1fd33c76adb8f0db4eb7b2de2efbae81b1aa732a
Rasmus Andersson <rasmus@notion.se>
2020-03-15 19:58:03 -0700

    dist script

--- misc/dist.sh
+++ misc/dist.sh
@@ -2,7 +2,13 @@
 cd "$(dirname "$0")/.."
 ./misc/build-plugin.sh
 ./misc/build-app.sh
+
+mkdir -p dist
+GITREV=$(git rev-parse --short=16 HEAD)
+pushd build/figma-plugin >/dev/null
+zip -q -X -r "../../dist/scripter-figma-plugin-${GITREV}.zip" *
+popd >/dev/null
+
 git add docs
 git status
-
 echo "Ready to commit & push"
